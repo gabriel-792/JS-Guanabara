6ec7baaa8a434448cf70e1d95ed88296afd2a4d6
gabriel-792 <gabriel.marques.avalo.da.silva@Outlook.com>
2026-07-21 19:18:27 -0300

    aula 12 exercicios completos

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,48 @@
+function verificar() {
+    var ano = new Date().getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if ( fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert(' [ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/criança-masculino-final.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'imagens/jovem-masculino-final.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/adulto-masculino-final.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/idoso-masculino-final.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/criança-feminino-final.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'imagens/jovem-feminino-final.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/adulto-feminino-final.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/idoso-feminino-final.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
