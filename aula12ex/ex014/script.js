003595d14338d9942a4dc4eb2e7f4af9746c653e
gabriel-792 <gabriel.marques.avalo.da.silva@Outlook.com>
2026-07-20 21:49:59 -0300

    vaientender cada coisa

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,6 +1,20 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
-    var horaAtual = new Date().getHours()
+    var data = new Date()
+    var horaAtual = data.getHours()
     msg.innerHTML = `Agora são ${horaAtual} horas`
+    if (horaAtual >= 0 && horaAtual < 12) {
+        // BOM DIA!
+        img.src = 'imagens/manhafinal.png'
+        document.body.style.background = '#9B6F31'
+    } else if (horaAtual >= 12 && horaAtual < 18) {
+        // BOA TARDE!
+        img.src = 'imagens/tardefinal.png'
+        document.body.style.background = '#DC364E'
+    } else {
+        // BOA NOITE!
+        img.src = 'imagens/noitefinal.png'
+        document.body.style.background = '#121332'
+    }
 }
