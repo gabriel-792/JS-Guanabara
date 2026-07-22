95bcb115b760b809652b36ddd18be201839be454
gabriel-792 <gabriel.marques.avalo.da.silva@Outlook.com>
2026-07-22 15:58:50 -0300

    aula 14 terminada

---
+++ aula13/node_f6846d48fda16.tmp.js
@@ -0,0 +1,19 @@
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+var cc = 1
+while (true) {
+    console.log(`Teste ${cc}`)
+    if (cc >= 6) {
+        break
+    }
+}
